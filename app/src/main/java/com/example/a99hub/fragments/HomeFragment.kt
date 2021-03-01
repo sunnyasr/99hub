66d0a51cc0aafe1055570d601a8ccd23eeb99620
sune786 <sunesingh65@gmail.com>
2021-03-01 12:40:50 +0530

    init project

---
+++ app/src/main/java/com/example/a99hub/fragments/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.a99hub.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a99hub.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
